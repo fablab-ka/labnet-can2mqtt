c67392c9ea9ce937fdf5e7528e271f89defac7c3
Sven Hecht <sven.hecht@logmein.com>
2018-03-09 18:48:53 +0100

    Add mqtt subscription

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     mqtt_broker = "192.168.1.6"
     mqtt_broker_port = "1883"
     mqtt_client_id = "bride_1"
-    mqtt_topic_template = "/FLKA/Steckdosen/Leiste%s/Dose%s/"
+    mqtt_topic_template = "/FLKA/Steckdosen/Leiste%s/Dose%s/%s"
 
     canbus_interface = "can0"
     canbus_type = "socketcan"
