7701f028f14b04ef1f9ee7bd9f5bae0b1e1db323
Sven Hecht <sven.hecht@citrix.com>
2018-03-12 09:45:12 +0100

    config wording fix

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config:
     mqtt_broker_port = "1883"
     mqtt_client_id = "bride_1"
     mqtt_topic_template = "/FLKA/Steckdosen/Leiste%02d/Dose%02d/%s"
-    mqtt_topic_iterator1_count = 4
-    mqtt_topic_iterator2_count = 4
+    mqtt_topic_iterator1_max = 4
+    mqtt_topic_iterator2_max = 4
     default_target_power_hub = 1
     
     mqtt_cmd_topic_iterators_regex = '^/FLKA/Steckdosen/Leiste([0-9]*)/Dose([0-9]*)/cmd/(.*)$'
